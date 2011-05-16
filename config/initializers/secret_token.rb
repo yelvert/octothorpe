15650f3789b93f3b8f81ba45a1341f2b59fbef54
Taylor Yelverton <taylor@synergy-solutions.biz>
2011-05-16 00:25:14 -0500

    Init Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Octothorpe::Application.config.secret_token = 'e56cddf5cf9dfd021f11bb63c345171aaa983feb63c22fd8d21f68b6e4b31072d910cc23ce24ac406b65a5e685ed702f93c53968ef92ab9e003d8a45875cd007'

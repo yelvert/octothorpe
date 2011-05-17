3fc5c23c353f6aa5afcfd9db9103f9b6988f7843
Taylor Yelverton <taylor@synergy-solutions.biz>
2011-05-17 18:16:58 -0400

    Made pages#show 404 if the page doesnt exist.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def login_required
     redirect_to(admin_login_path) unless session[:user]
   end
+  
+  def page_not_found
+    render :file => "#{Rails.root}/public/404.html", :status => :not_found, :layout => false
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
   def show
     @page = Page.where(:title_for_url => (params[:title] || 'home')).first
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @page }
-    end
+    page_not_found
   end
 end

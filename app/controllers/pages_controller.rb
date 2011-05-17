a42b0d6f7003401cd72b0f2d73704106a5ed4084
Taylor Yelverton <taylor@synergy-solutions.biz>
2011-05-17 18:18:57 -0400

    Left off a nil check.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def show
     @page = Page.where(:title_for_url => (params[:title] || 'home')).first
-    page_not_found
+    page_not_found if @page.nil?
   end
 end

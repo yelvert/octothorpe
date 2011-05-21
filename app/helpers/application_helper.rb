de5fbc8f62e81c36a4937419a852dc8ff1a2d6d4
Taylor Yelverton <taylor@synergy-solutions.biz>
2011-05-21 00:07:54 -0400

    Move google analytics to a helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
   def logged_in?
     session[:user_id] ? true : false
   end
+  
+  def google_analytics(account_number)
+    %Q[
+      <script type="text/javascript" charset="utf-8">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', '#{account_number}']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+      </script>
+    ]
+  end
 end
